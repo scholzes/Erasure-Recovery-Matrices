ae10e20f744be1abdf9d41320683c77da03d0f55
Sam Scholze <scholzes@tamu.edu>
2017-04-18 11:52:01 -0500

    Added NoiseExperiment.m and modified IRSexperiment.m

--- NoiseExperiment.m
+++ NoiseExperiment.m
@@ -6,7 +6,7 @@ fprintf('Reading Image \n');
 
 COMPRESSION_PERCENT = 0.03; % Compressed Signal will be approximately
 % n = 256^2 * COMPRESSION_PERCENT dimensional.
-% percenterasures = .01;
+percenterasures = .05;
 
 Original_Image_Double = double(imread('Lena.bmp'));
 
@@ -19,6 +19,8 @@ Compressed_Image_Double(I(n+1:256*256)) = [];
 
 N = 2*n+1000;
 m = 1000;
+L = [1:round(percenterasures(j)*N)];
+LC = setdiff(1:N,L);
 
 f = Compressed_Image_Double;
 
@@ -31,19 +33,9 @@ DF = sqrt(N/n)*A(:,1:n)';
 EF = sqrt(n/N)*A(:,n+1:2*n)' + (n/N)*DF;
 M = sqrt(N/m)*A(:,2*n+1:2*n+m)';
 
-fprintf('Creating More Frames \n');
-
-A1 = randn(N,2*n+m);
-A1(:,1:n) = DF';
-[A1,~] = qr(A1,0);
-
-DF1 = sqrt(N/n)*A1(:,1:n)';
-EF1 = sqrt(n/N)*A1(:,n+1:2*n)' + (n/N)*DF1;
-M1 = sqrt(N/m)*A1(:,2*n+1:2*n+m)';
+snr = [.01, .05, .10, .15, .2]
 
 fprintf('Reconstructing Erasures \n');
-
-percenterasures = [.01, .03, .05];
 
 figure;
 
@@ -58,22 +50,23 @@ subplot(3,3,2);
 imshow(J_f);
 title('Compressed Image');
 
-for(j = 1:1:3)
-
-    L = [1:round(percenterasures(j)*N)];
+for(j = 1:1:length(snr))
 
     FC = EF' * f;
     FC(L) = zeros(size(L'));
-    FC1 = FC;
+    noise = randn(length(LC));
+    noise = noise / norm(noise) * 
+    FC(LC) = FC(LC) + noise;
     f_R = DF*FC;
 
-    LC = setdiff(1:N,L);
     FC(L) = -(M(:,L)' * M(:,L)) \ (M(:,L)' * (M(:,LC) * FC(LC)));
     g = f_R + DF(:,L) * FC(L);
 
-    FC1(L) = -(M1(:,L)' * M1(:,L))\(M1(:,L)' * (M1(:,LC) * FC1(LC)));
-
-    g1 = f_R + DF(:,L) * FC1(L);
+    C_f_R = zeros(256*256,1); % Erased Image with noise.
+    C_f_R(I1(1:n)) = f_R;
+    Uncompressed_f_R = ifft(C_f_R);
+    Uncompressed_f_R = reshape(Uncompressed_f_R,[256,256]);
+    J_f_R = uint8(Uncompressed_f_R);
 
     C_g = zeros(256*256,1); % Reconstructed Image.
     C_g(I1(1:n)) = g;
@@ -81,18 +74,12 @@ for(j = 1:1:3)
     Uncompressed_g = reshape(Uncompressed_g,[256,256]);
     J_g = uint8(Uncompressed_g);
 
-    C_g1 = zeros(256*256,1); % Reconstructed Image.
-    C_g1(I1(1:n)) = g1;
-    Uncompressed_g1 = ifft(C_g1);
-    Uncompressed_g1 = reshape(Uncompressed_g1,[256,256]);
-    J_g1 = uint8(Uncompressed_g1);
-
     subplot(3,3,3+j);
-    imshow(J_g);
-    title('Correct Erasure Recovery Matrix');
+    imshow(J_f_R);
+    title('Erased Image with Noise');
 
     subplot(3,3,5+j);
-    imshow(J_g1);
-    title('Incorrect Erasure Recovery Matrix');
+    imshow(J_g);
+    title('Reconstructed with Noise');
 
 end
