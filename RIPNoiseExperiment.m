3d57494bb02adf96d3665d5c4681ebaf2c905ca9
Sam Scholze <scholzes@bloc610a-01.math.tamu.edu>
2017-04-19 14:21:52 -0500

    Added Spreadsheets for the Paper.

--- RIPNoiseExperiment.m
+++ RIPNoiseExperiment.m
@@ -1,22 +1,22 @@
-m = 200;
-n = 200;
+m = 100;
+n = 100;
 N = 1000;
 snr = .05;
 Trials = 50;
-EC = [5:5:20];
+EC = [5:5:100];
 
 Data = zeros(Trials,length(EC));
+Data2 = zeros(Trials,length(EC));
 
 for(k=1:1:length(EC))
     
     for(t = 1:1:Trials)
         
-        L = [1:1:k];
+        L = [1:1:EC(k)];
+        LC = setdiff(1:N,L);
         
         A = randn(N,2*n+m);
-        
         [A,~] = qr(A,0);
-        
         DF = sqrt(N/n)*A(:,1:n)';
         EF = sqrt(n/N)*A(:,n+1:2*n)' + (n/N)*DF;
         M = sqrt(N/m)*A(:,2*n+1:2*n+m)';
@@ -25,9 +25,9 @@ for(k=1:1:length(EC))
         f = f./norm(f,2);
         
         FC = EF' * f;
-        noise = randn(N,1);
-        noise = snr/norm(noise) * noise;
-        FC = FC + noise;
+        noise = randn(length(LC),1);
+        noise = snr * norm(FC(LC))/norm(noise) * noise;
+        FC(LC) = FC(LC) + noise;
         FC(L) = zeros(size(L'));
         f_R = DF*FC;
         
@@ -37,6 +37,7 @@ for(k=1:1:length(EC))
         g = f_R + DF(:,L) * FC(L);
         
         Data(t,k) = norm(f-g);
+        Data2(t,k) = norm(f-f_R);
         
     end
     
@@ -44,10 +45,14 @@ for(k=1:1:length(EC))
 
 end
 
-X = repmat(EC,Trials);
+X = repmat(EC,Trials,1);
 X = reshape(X,[length(EC)*Trials,1]);
 Y = reshape(Data,[length(EC)*Trials,1]);
+Z = reshape(Data2,[length(EC)*Trials,1]);
 plot(X,Y,'x')
 hold on;
 plot(EC,median(Data));
+title('Erasure Set Size vs Reconstruction Error');
+xlabel('Erasure Set Size');
+ylabel('Reconstruction Error');
 hold off;
